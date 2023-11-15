60dca7d18e92fee29aab084daee70af1aa01a4f1
bartoszkapciak <bartosz.kapciak@swmansion.com>
2023-11-15 12:35:33 +0100

    check ci

--- packages/react/src/SwmIcon.tsx
+++ packages/react/src/SwmIcon.tsx
@@ -1,3 +1,3 @@
 export function SwmIcon() {
-  return <div style={{ width: '50px', height: '50px', backgroundColor: 'red' }}></div>;
+  return <div style={{ width: '50px', height: '50px', backgroundColor: 'darkblue' }}></div>;
 }
